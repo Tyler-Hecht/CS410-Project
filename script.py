f5da9c4175be6ffeedb783392a6efb81d3ef42c7
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2023-12-14 20:09:00 -0600

    data

--- script.py
+++ script.py
@@ -1,14 +1,23 @@
 from js import document
 from pyodide.ffi import create_proxy
 from query import query
-import numpy as np
 
 def runPython(*args, **kwargs):
     query_text = document.getElementById("query").value
     results = query(query_text)
     document.getElementById("results").innerHTML = ""
+    output = ""
     for course, score in results:
-        document.getElementById("results").innerHTML += f"<p>{course} ({score})</p>"
+        url = f"https://cs.illinois.edu/academics/courses/{course.split()[0]}{course.split()[1]}"
+        # course and score only (no url)
+        output += f"{course} ({score})<br>"
+        # url in smaller font
+        output += f"<a href=\"{url}\" style=\"font-size: 0.8em;\">{url}</a><br>"
+    if output:
+        document.getElementById("results").innerHTML = output
+    else:
+        document.getElementById("results").innerHTML = "<p>No results found.</p>"
+
 
 # function_proxy = create_proxy(runPython)
 
